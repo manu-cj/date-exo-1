52ff747dfa1db97965c20616709179eb99bf0805
manu-cj <carenjotje@gmail.com>
2024-05-03 12:30:44 +0200

    save and done exo 2

---
+++ app.js
@@ -0,0 +1,39 @@
+
+let body = document.querySelector('body');
+
+
+
+function createDateUtc(params) {
+    let h3 = document.createElement('h3');
+    const now = new Date()
+    const options = { timeZone: params };
+    if (params === 'UTC') {
+        h3.textContent = `${now.toLocaleString('en-US', options)} à Iceland/Reykjavik `;
+    }else {
+        h3.textContent = `${now.toLocaleString('en-US', options)} à ${params}`;
+    }
+    body.appendChild(h3);
+}
+
+createDateUtc('America/Anchorage');
+createDateUtc('UTC');
+createDateUtc('Europe/Moscow');
+createDateUtc('Europe/Brussels');
+
+let myBirthday = new Date('1995', '10', '29')
+console.log(myBirthday.getTime() / (1000 * 60 * 60 * 24));
+
+let h3 = document.createElement('h3');
+body.appendChild(h3);
+h3.textContent = myBirthday.getTime() / (1000 * 60 * 60 * 24) + ' Day after my birthday';
+
+
+function dayAfterThisDate(year, month, day,) {
+    let date = new Date(year, month, day);
+    let calc = date.getTime() / (1000 * 60 * 60 * 24);
+    let h3 = document.createElement('h3');
+    body.appendChild(h3);
+    h3.textContent = calc + ' Day after this date ' + year + ' ' + month+  ' ' + day;
+}
+
+dayAfterThisDate('1970', '01', '01');
